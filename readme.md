39dd614e08743267cfec56ab806413493982b87e
Henrique Melo <hacmelo@gmail.com>
2023-05-17 09:10:27 -0300

    add download aulas pdf and code refactor

--- readme.md
+++ readme.md
@@ -1,12 +1,12 @@
 # Automação Grancursos
 
-  Essa aplicação feita em Python faz o download de todos os slides e degravações de cada aula de todas as matérias de um curso. Todo o conteudo é gerado na pasta "content" e fica separado em pastas com o nome das disciplinas e aulas.
+  Essa aplicação feita em Python faz o download de todos as aulas PDF, slides e degravações de cada aula de todas as matérias de um curso. Todo o conteudo é gerado na pasta "content" e fica separado em pastas com o nome das disciplinas e aulas.
 
 ## Variáveis de ambiente
 
-É preciso criar uma cópia do arquivo .env.example, renomea-lo para .env e definir os valores das variáveis CURSO_ID e COOKIE_VALUE.
+É preciso criar uma cópia do arquivo .env.example, renomea-lo para .env e definir os valores das variáveis CURSO_ID e AUTH_COOKIE_VALUE.
 
-### Cookie de autenticação (COOKIE_VALUE)
+### Cookie de autenticação (AUTH_COOKIE_VALUE)
  Para definir o cookie de autenticação é necessário estar autenticado na sua conta do grancursos. Depois disso basta abrir as ferramentas de desenvolvedor do seu navegador(Control + Shift + I no Windows / Command + Option + I no Mac) e copiar o valor(da coluna value) do cookie "grancursosonline" assim como na imagem abaixo:
 ![Cookie](./assets/cookie_value.png)
 
--- script.py
+++ script.py
@@ -2,9 +2,106 @@ import os
 import requests
 from http.cookiejar import CookieJar
 import time
-import shutil
 import urllib.parse
+import shutil
 from dotenv import load_dotenv
+
+
+load_dotenv()
+cookie_jar = CookieJar()
+
+CURSO_ID = os.getenv("CURSO_ID")
+BASE_URL = os.getenv("BASE_URL")
+DISCIPLINAS_URL = BASE_URL + os.getenv("DISCIPLINAS_URL").replace("CURSO_ID", CURSO_ID)
+CONTEUDOS_URL = DISCIPLINAS_URL + os.getenv("CONTEUDOS_URL")
+DOWNLOAD_AULA_URL = BASE_URL + os.getenv("DOWNLOAD_AULA_URL")
+DISCIPLINAS_PDF_URL = BASE_URL + os.getenv("DISCIPLINAS_PDF_URL").replace(
+    "CURSO_ID", CURSO_ID
+)
+CONTEUDOS_PDF_URL = DISCIPLINAS_PDF_URL + os.getenv("CONTEUDOS_PDF_URL")
+DOWNLOAD_AULA_PDF_URL = BASE_URL + os.getenv("DOWNLOAD_AULA_PDF_URL")
+AUTH_COOKIE_NAME = os.getenv("AUTH_COOKIE_NAME")
+AUTH_COOKIE_VALUE = os.getenv("AUTH_COOKIE_VALUE")
+
+
+def main():
+    if not CURSO_ID or not AUTH_COOKIE_VALUE:
+        print("Preencha corretamente as variáveis de ambiente! (.env)")
+        return
+
+    operacao, apagar = draw_menu()
+    countdown()
+    handle_folders(apagar == 1)
+    set_auth_cookie()
+
+    if not operacao == 2:
+        process_disciplinas_pdf()
+
+    if not operacao == 1:
+        process_disciplinas()
+
+    print("\n\nProcessamento Finalizado!")
+
+
+def set_auth_cookie():
+    cookie = requests.cookies.create_cookie(
+        name=AUTH_COOKIE_NAME, value=AUTH_COOKIE_VALUE
+    )
+    cookie_jar.set_cookie(cookie)
+
+
+def handle_folders(apagar):
+    if apagar and os.path.exists("content"):
+        shutil.rmtree("content")
+
+    if not os.path.exists("content"):
+        os.mkdir("content")
+    with open("errors.txt", "w") as file:
+        file.write(f"")
+
+
+def clear_terminal():
+    os.system("cls" if os.name == "nt" else "clear")
+
+
+def draw_menu():
+    clear_terminal()
+    print("|========= Automação Grancursos =========|")
+    print("|                                        |")
+    print("| Qual operação deseja realizar?         |")
+    print("|  1. Download das aulas em PDF          |")
+    print("|  2. Download de slides e degravações   |")
+    print("|  3. Download de ambos                  |")
+    print("|                                        |")
+    print("|========================================|")
+
+    while True:
+        operacao = input("")
+        if operacao in (1, 2, 3, "1", "2", "3"):
+            break
+        else:
+            print("Opção inválida. Por favor, escolha uma opção válida.")
+
+    clear_terminal()
+
+    print("|========= Automação Grancursos =========|")
+    print("|                                        |")
+    print("| Deseja apagar os conteúdos anteriores? |")
+    print("|  1. Sim                                |")
+    print("|  2. Não                                |")
+    print("|                                        |")
+    print("|========================================|")
+
+    while True:
+        apagar = input("")
+        if apagar in (1, 2, "1", "2"):
+            break
+        else:
+            print("Opção inválida. Por favor, escolha uma opção válida.")
+
+    clear_terminal()
+
+    return int(operacao), int(apagar)
 
 
 def countdown():
@@ -15,150 +112,133 @@ def countdown():
     print("Processamento iniciado!")
 
 
-def handle_folders():
-    if os.path.exists("content"):
-        shutil.rmtree("content")
-    os.mkdir("content")
-    with open("errors.txt", "w") as file:
-        file.write(f"")
+def process_disciplinas():
+    print("\nIniciando download dos slides e degravações.")
+    disciplinas = request_json(DISCIPLINAS_URL)
+    handle_disciplinas(disciplinas, "Slides e Degravações", handle_aulas, CONTEUDOS_URL)
+    print("Download dos slides e degravações concluído.")
 
 
-def set_auth_cookie():
-    cookie = requests.cookies.create_cookie(name=COOKIE_NAME, value=COOKIE_VALUE)
-    cookie_jar.set_cookie(cookie)
+def process_disciplinas_pdf():
+    print("\nIniciando download das aulas em pdf.")
+    disciplinas_pdf = request_json(DISCIPLINAS_PDF_URL)
+    handle_disciplinas(
+        disciplinas_pdf, "Aulas em PDF", handle_aulas_pdf, CONTEUDOS_PDF_URL
+    )
+    print("Download das aulas em pdf concluído.")
+
+
+def handle_disciplinas(disciplinas, path, callback_aulas, base_url):
+    new_prefix = create_folder("content/", path)
+
+    for disciplina in disciplinas:
+        disciplina_id = disciplina["id"]
+        disciplina_name = disciplina["nome"]
+
+        new_prefix_disciplina = create_folder(new_prefix, disciplina_name)
+        url = base_url.replace("DISCIPLINA_ID", disciplina_id)
+        conteudos = request_json(url)
+        if conteudos:
+            handle_conteudos(url, new_prefix_disciplina, conteudos, callback_aulas)
+
+
+def handle_conteudos(url, prefix, conteudos, callback_aulas):
+    count = 1
+    for conteudo in conteudos:
+        new_prefix = create_folder(prefix, f"Aula {count}")
+        count += 1
+
+        aulas = request_json(f"{url}{conteudo['id']}")
+        if aulas:
+            callback_aulas(new_prefix, aulas)
+
+
+def handle_aulas(prefix, aulas):
+    count = 1
+    for aula in aulas:
+        fk_apostila = aula["fk_apostila"]
+        fk_resumo = aula["fk_material_resumo"]
+        aula_name = f"{count} {aula['st_titulo_novo']}"
+        count += 1
+        new_prefix = create_folder(prefix, aula_name)
+        handle_slide_or_resumo(fk_apostila, new_prefix, "slide")
+        handle_slide_or_resumo(fk_resumo, new_prefix, "resumo")
+        print("\n\n")
+
+
+def handle_slide_or_resumo(fk, path, type_name):
+    if fk and not pdf_exists(path, type_name):
+        url = DOWNLOAD_AULA_URL.replace("TYPE", type_name).replace("FK", encode(fk))
+        pdfContent = request_pdf(url)
+        save_pdf(pdfContent, path, type_name)
+        print(f"{path}{type_name}.pdf salvo!")
+    else:
+        print(f"{path}{type_name}.pdf já existe!")
+
+
+def handle_aulas_pdf(prefix, aulas_pdf):
+    count = 1
+    for aula_pdf in aulas_pdf:
+        aula_pdf_id = encode(aula_pdf["id_aula_pdf"])
+        aula_pdf_name = f"{count} {aula_pdf['st_titulo']}"
+        count += 1
+
+        if pdf_exists(prefix, aula_pdf_name):
+            print(f"{prefix}{aula_pdf_name} - já existe!\n\n")
+            continue
+
+        url = DOWNLOAD_AULA_PDF_URL.replace("AULA_PDF_ID", aula_pdf_id)
+        pdf = request_pdf(url)
+        save_pdf(pdf, prefix, aula_pdf_name)
+        print(f"{prefix}{aula_pdf_name} - salvo!\n\n")
+
+
+def pdf_exists(path, filename):
+    filename = handle_illegal_characters(filename)
+    return os.path.exists(path + filename + ".pdf")
+
+
+def request_json(url):
+    try:
+        response = requests.get(url, cookies=cookie_jar)
+        return response.json()
+    except Exception as e:
+        with open("errors.txt", "a") as file:
+            file.write(f"{url} - {e}\n")
+        return []
+
+
+def request_pdf(url):
+    try:
+        response = requests.get(url, cookies=cookie_jar)
+        return response.content
+    except Exception as e:
+        with open("errors.txt", "a") as file:
+            file.write(f"{url} - {e}\n")
+        return bytearray
+
+
+def save_pdf(pdf, path, filename):
+    filename = handle_illegal_characters(filename)
+    with open(os.path.join(path, filename + ".pdf"), "wb") as f:
+        f.write(pdf)
+
+
+def create_folder(prefix, path):
+    path = prefix + handle_illegal_characters(path)
+    if not os.path.exists(path):
+        os.mkdir(path)
+    return path + "/"
+
+
+def handle_illegal_characters(value):
+    illegal_chars = ["<", ">", ":", '"', "/", "\\", "|", "?", "*"]
+    return "".join(c if c not in illegal_chars else " " for c in value)
 
 
 def encode(value):
     return urllib.parse.quote(value, safe=" ")
 
-
-def request_file(download_type, fk, aula_name):
-    fk_encoded = encode(fk)
-    url = URL_AULAS.replace("DOWNLOAD_TYPE", download_type).replace(
-        "FK_ENCODED", fk_encoded
-    )
-    retry_count = 0
-    retry_limit = 3
-
-    while retry_count < retry_limit:
-        response = requests.get(url, cookies=cookie_jar)
-        if response.status_code == 200 and len(response.content) > 0:
-            return response.content
-        else:
-            print(f"Erro em {url}, tentando novamente...")
-            time.sleep(1)
-            retry_count += 1
-
-    with open("errors.txt", "a") as file:
-        file.write(f"\nErro ao baixar {download_type} de {aula_name} em {url}")
-    print(f"Não foi possível baixar {url} depois de {retry_limit} tentativas.")
-    return bytearray()
-
-
-def handle_illegal_characters(value):
-    illegal_chars = ["<", ">", ":", '"', "/", "\\", "|", "?", "*"]
-    return "".join(c if c not in illegal_chars else "_" for c in value)
-
-
-def save_file(content, disciplina_name, aula_name, conteudo_name, filename):
-    aula_name = handle_illegal_characters(aula_name)
-    disciplina_name = handle_illegal_characters(disciplina_name)
-
-    disciplina_path = f"content/{disciplina_name}"
-    conteudo_path = f"{disciplina_path}/{conteudo_name}"
-    aula_path = f"{conteudo_path}/{aula_name}"
-
-    if not os.path.exists(disciplina_path):
-        os.mkdir(disciplina_path)
-
-    if not os.path.exists(conteudo_path):
-        os.mkdir(conteudo_path)
-
-    if not os.path.exists(aula_path):
-        os.mkdir(aula_path)
-
-    with open(os.path.join(aula_path, filename + ".pdf"), "wb") as f:
-        f.write(content)
-
-
-def handle_aulas(aulas, disciplina_name, conteudo_name):
-    count_aula = 1
-    for aula in aulas:
-        aula_name = f"{count_aula} {aula['st_titulo_novo']}"
-        fk_apostila = aula["fk_apostila"]
-        fk_resumo = aula["fk_material_resumo"]
-
-        if fk_apostila:
-            pdfContent = request_file("apostila", fk_apostila, aula_name)
-            save_file(pdfContent, disciplina_name, aula_name, conteudo_name, "slide")
-
-        if fk_resumo:
-            pdfContent = request_file("resumo", fk_resumo, aula_name)
-            save_file(pdfContent, disciplina_name, aula_name, conteudo_name, "resumo")
-
-        print(f"{disciplina_name} - {conteudo_name} - {aula_name} salvo!\n")
-        count_aula += 1
-
-
-def handle_conteudos(conteudos, disciplina_id, disciplina_name):
-    for conteudo in conteudos:
-        conteudo_id = conteudo["id"]
-        aulas = get_aulas(disciplina_id, conteudo_id)
-        handle_aulas(aulas, disciplina_name, f"Aula {conteudo_id}")
-
-
-def handle_disciplinas(disciplinas):
-    for disciplina in disciplinas:
-        disciplina_id = disciplina["id"]
-        disciplina_name = disciplina["nome"]
-
-        conteudos = get_conteudos(disciplina_id)
-        handle_conteudos(conteudos, disciplina_id, disciplina_name)
-
-
-def get_aulas(disciplina_id, conteudo_id):
-    url = (
-        URL_DISCIPLINA.replace("CURSO_ID", CURSO_ID)
-        .replace("DISCIPLINA_ID", disciplina_id)
-        .replace("CONTEUDO_ID", f"{conteudo_id}")
-    )
-
-    response = requests.get(url, cookies=cookie_jar)
-    return response.json()
-
-
-def get_conteudos(disciplina_id):
-    url = URL_CONTEUDO.replace("CURSO_ID", CURSO_ID).replace(
-        "DISCIPLINA_ID", disciplina_id
-    )
-    response = requests.get(url, cookies=cookie_jar)
-    return response.json()
-
-
-def get_disciplinas():
-    url = URL_CURSO.replace("CURSO_ID", CURSO_ID)
-    response = requests.get(url, cookies=cookie_jar)
-    return response.json()
-
-
-def main():
-    countdown()
-    handle_folders()
-    set_auth_cookie()
-    disciplinas = get_disciplinas()
-    handle_disciplinas(disciplinas)
-    print("Processamento finalizado!")
-
-
-cookie_jar = CookieJar()
-load_dotenv()
-CURSO_ID = os.getenv("CURSO_ID")
-URL_CURSO = os.getenv("URL_CURSO")
-URL_CONTEUDO = os.getenv("URL_CONTEUDO")
-URL_DISCIPLINA = os.getenv("URL_DISCIPLINA")
-URL_AULAS = os.getenv("URL_AULAS")
-COOKIE_NAME = os.getenv("COOKIE_NAME")
-COOKIE_VALUE = os.getenv("COOKIE_VALUE")
 
 if __name__ == "__main__":
     main()
